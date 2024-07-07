ec902a8a181dc0bb030609b43afae13bf7900c95
Namima Islam <namimaislam308@gmail.com>
2024-07-07 15:47:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//.reduce() - reduces the elements of an array to a single value
+//should have a `totalBatteries` variable
+//should have a number as a result
+//should have made the sum of all the assembled batteries
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(totalBatteries);
+
+//function getSum(accumulator, element) {
+//    return Math.
+//}
